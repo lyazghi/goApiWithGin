d0102ecf011321c11822cf3d8d0e7495ac4a31a8
naslyazghi <nas.lyazghi@knights.ucf.edu>
2024-05-16 13:42:44 -0400

    Added more crud functions

--- main.go
+++ main.go
@@ -9,6 +9,9 @@ import (
 func main() {
 	router := gin.Default()
 	router.GET("/books", repositories.GetBooks)
+	router.GET("/books/:id", repositories.BookById)
 	router.POST("/books", repositories.CreateBook)
+	router.PATCH("/checkout", repositories.CheckoutBook)
+	router.PATCH("/return", repositories.ReturnBook)
 	router.Run("localhost:8080")
 }
--- repositories/bookRepository.go
+++ repositories/bookRepository.go
@@ -1,6 +1,7 @@
 package repositories
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -23,6 +24,18 @@ func GetBooks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, books)
 }
 
+func BookById(c *gin.Context) {
+	id := c.Param("id")
+	book, err := getBookById(id)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found."})
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, book)
+}
+
 func CreateBook(c *gin.Context) {
 	var newBook book
 
@@ -32,4 +45,58 @@ func CreateBook(c *gin.Context) {
 
 	books = append(books, newBook)
 	c.IndentedJSON(http.StatusCreated, newBook)
+}
+
+func ReturnBook(c *gin.Context) {
+	id, ok := c.GetQuery("id")
+
+	if !ok {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing id query parameter."})
+		return
+	}
+
+	book, err := getBookById(id)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found."})
+		return
+	}
+
+	book.Quantity += 1
+	c.IndentedJSON(http.StatusOK, book)
+}
+
+func CheckoutBook(c *gin.Context) {
+	id, ok := c.GetQuery("id")
+
+	if !ok {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing id query parameter."})
+		return
+	}
+
+	book, err := getBookById(id)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found."})
+		return
+	}
+
+	if book.Quantity <= 0 {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Book not available."})
+		return
+	}
+
+	book.Quantity -= 1
+	c.IndentedJSON(http.StatusOK, book)
+}
+
+// Helper functions
+func getBookById(id string) (*book, error) {
+	for i, b := range books {
+		if b.ID == id {
+			return &books[i], nil
+		}
+	}
+
+	return nil, errors.New("book not found")
 }
